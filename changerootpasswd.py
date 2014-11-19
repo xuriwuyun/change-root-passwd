5d7863926cb6b3cbd1cb64767ecbaa890e6608d7
xuriwuyun <xuriwuyun@gmail.com>
2014-11-19 18:09:22 +0800

    Update changerootpasswd.py

--- changerootpasswd.py
+++ changerootpasswd.py
@@ -1,16 +1,12 @@
-# Copyright 2014 Red Hat, Inc.                                                  
+#!/usr/bin/env bash                                                             
+# vim: tabstop=4 shiftwidth=4 softtabstop=4                                     
 #                                                                               
-# Licensed under the Apache License, Version 2.0 (the "License"); you may       
-# not use this file except in compliance with the License. You may obtain       
-# a copy of the License at                                                      
+# Copyright 2013 www.jd.com                                                     
 #                                                                               
-# http://www.apache.org/licenses/LICENSE-2.0                                    
-#                                                                               
-# Unless required by applicable law or agreed to in writing, software           
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT     
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the      
-# License for the specific language governing permissions and limitations       
-# under the License.                                                            
+# File: changerootpasswd.sh                                                     
+# Author: xuriwuyun <xuriwuyun@gmail.com>                                       
+# Created: 11/19/2014 15:36:21                                                  
+# Last_modified: 11/19/2014 15:45:46                                                            
                                                                                 
 import six                                                                      
 import sys                                                                      
